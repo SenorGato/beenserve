0ff1774ed193813ae8a33fbe1f77e425e0dc5e60
SenorGato <senoraraton@gmail.com>
2023-01-25 18:33:31 -0800

    Changed test data, and inited image render in index.js

--- data/test_tlc_product_data.sql
+++ data/test_tlc_product_data.sql
@@ -1,11 +1,9 @@
 DO $$
     BEGIN
         INSERT INTO tealacarte.products (name, price, SKU, path)
-        VALUES ('test4', 19, '0123456791abc', '/home'),
-               ('test5', 18, '0123456792abc', '/home'),
-               ('test6', 17, '0123456793abc', '/home'),
-               ('test7', 16, '0123456794abc', '/home'),
-               ('test8', 12, '0123456795abc', '/home');
+        VALUES ('golang', 10, '0123456791abc', '/views/assets/gopher.jpg'),
+               ('psql', 100, '0123456792abc', '/views/assets/psql.jpg'),
+               ('docker', 1000, '0123456793abc', '/views/assets/Moby-logo.png');
         EXCEPTION WHEN undefined_table THEN
             RAISE NOTICE 'tealacarte.products undef.  Please init';
     END;
--- views/js/index.js
+++ views/js/index.js
@@ -4,9 +4,17 @@ async function fetchProducts() {
     return products;
 }
 
+function display_product(src) {
+    var img = document.createElement("img");
+    img.src = src;
+    document.body.appendChild(img);
+}
+
 async function run() {
     let products = await fetchProducts()
-    console.log(products);
+    show_image(products[0].image_path);
+    //console.log(products);
+    //console.log(products[0]);
         let list = document.createElement("ul");
         for (let x of products) {
             var item = document.createElement("li"); 
